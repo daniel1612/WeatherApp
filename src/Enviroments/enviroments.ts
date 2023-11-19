dee14c663867257d2c8e96b667c99bc0990303e8
daniel1612 <danieldahan220715@gmail.com>
2023-11-19 19:28:24 +0200

    roni12

--- src/Enviroments/enviroments.ts
+++ src/Enviroments/enviroments.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     apiKey: 'iq74AwD0cAxU5oLs3FD6MUzD1YbvPTNv',
     // apiKey: 'MgZ3CDmbS362HuXhKLytVGSlOfLoeUeo',
-    urlLoaction: 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=',
-    urlCurrent: 'http://dataservice.accuweather.com/currentconditions/v1/',
-    urlFive: 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/',
+    urlLoaction: 'https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=',
+    urlCurrent: 'https://dataservice.accuweather.com/currentconditions/v1/',
+    urlFive: 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/',
     language : '&language=en-us'
 }
